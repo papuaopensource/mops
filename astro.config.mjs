fb10bf742612d77d5d0a293ff1998b5241fa648b
antheiz <salupacode@gmail.com>
2025-04-22 14:35:29 +0900

    fix: set alias for react-dom/server directly in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,9 +12,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     resolve: {
-      alias: import.meta.env.PROD
-        ? { 'react-dom/server': 'react-dom/server.edge' }
-        : undefined,
+      alias: {
+        'react-dom/server': 'react-dom/server.edge', // Directly set the alias
+      },
     },
   },
 
