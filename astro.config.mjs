68f7bb9aec069da2d02c99517dacfe1ef099496d
antheiz <salupacode@gmail.com>
2025-04-22 14:29:35 +0900

    fix: add missing newline at the end of astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
-
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://mops.web.id',
   trailingSlash: 'always',
   output: 'server',
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: import.meta.env.PROD
+        ? { 'react-dom/server': 'react-dom/server.edge' }
+        : undefined,
+    },
   },
 
   integrations: [react()],
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
